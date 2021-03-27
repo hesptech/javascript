e0808d6f1ee410213359fc2c606ec837755bae40
hesptech <olea.oswaldo@gmail.com>
2021-03-27 21:30:44 +0100

    js if else

---
+++ js/if_else.js
@@ -0,0 +1,10 @@
+function (p.age) age: Number {
+    if (p.age == null) return
+    if (p.age >= 21) return
+    if (p.age < 18) return
+    if (p.age < 21) return
+}
+
+const p = {
+    age: 28,
+}
